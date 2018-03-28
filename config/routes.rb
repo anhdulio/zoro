274a15b35e7d4304f15f32690b7081721ff485e9
Anh Du Lio <lio.anhdu@gmail.com>
2018-03-28 16:11:33 +0700

    update routing

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
 
   # Render site
   mount Locomotive::Steam.to_app => '/_amp/', anchor: false
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
