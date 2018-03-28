7ce2e5469adeb0b1dcf3e4b08b7ea42160a211ca
Anh Du Lio <lio.anhdu@gmail.com>
2018-03-28 14:49:09 +0700

    change routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   mount Locomotive::API.to_app => '/locomotive(/:site_handle)/api'
 
   # Render site
-  mount Locomotive::Steam.to_app => '/', anchor: false
+  mount Locomotive::Steam.to_app => '/_amp/', anchor: false
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
